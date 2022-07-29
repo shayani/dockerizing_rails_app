7a87ae065768527f9afae901702143ec8a825fd1
Fernando Shayani <shayani@gmail.com>
2022-07-29 13:04:36 +0000

    Contact model and samples

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+20.times do
+  Contact.new.tap do |contact|
+    contact.name = Faker::Name.name
+    contact.age = rand(20..60)
+    contact.phone = Faker::PhoneNumber.phone_number
+    contact.address = Faker::Address.street_address
+    contact.save
+  end
+end
